f0fe9ab853125e58128fb1c8ab3f996d57c0ffdb
Dan <35927536+dan-mba@users.noreply.github.com>
2020-08-08 11:22:43 -0400

    change theme

--- download.sh
+++ download.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 curl --fail --silent \
   -o stats.svg \
-  "https://github-readme-stats.vercel.app/api?username=dan-mba&show_icons=true&title_color=006db0&icon_color=006db0"
+  "https://github-readme-stats.vercel.app/api?username=dan-mba&show_icons=true&theme=tokyonight"
 stats=$?
 if [ $stats -gt 0 ]
 then
@@ -9,7 +9,7 @@ then
 fi
 curl --fail --silent \
   -o langs.svg \
-  "https://github-readme-stats.vercel.app/api/top-langs/?username=dan-mba&layout=compact&hide=C%23&title_color=006db0"
+  "https://github-readme-stats.vercel.app/api/top-langs/?username=dan-mba&layout=compact&hide=C%23&theme=tokyonight"
 langs=$?
 if [ $langs -gt 0 ]
 then
