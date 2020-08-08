0b4fa03d24277918b7fe720d66652256ae4a0fcf
Dan <35927536+dan-mba@users.noreply.github.com>
2020-08-08 11:09:21 -0400

    fix if spacing

--- download.sh
+++ download.sh
@@ -3,7 +3,7 @@ curl --fail --silent \
   -o stats.svg \
   "https://github-readme-stats.vercel.app/api?username=dan-mba&show_icons=true&title_color=006db0&icon_color=006db0"
 stats=$?
-if [$stats -gt 0]
+if [ $stats -gt 0 ]
 then
   echo "Curl for stat failed with rc $stats"
 fi
@@ -11,7 +11,7 @@ curl --fail --silent \
   -o langs.svg \
   "https://github-readme-stats.vercel.app/api/top-langs/?username=dan-mba&layout=compact&hide=C%23&title_color=006db0"
 langs=$?
-if [$langs -gt 0]
+if [ $langs -gt 0 ]
 then
   echo "Curl for langs failed with rc $langs"
 fi
