13a5d5072544b2b4def2752d49b8f4b1fbdf5ae7
Dan <35927536+dan-mba@users.noreply.github.com>
2020-08-08 11:06:00 -0400

    udate download script

--- download.sh
+++ download.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
-curl --fail --silent -o stats.svg "https://github-readme-stats.vercel.app/api?username=dan-mba&show_icons=true&title_color=006db0&icon_color=006db0"
+curl --fail --silent \
+  -o stats.svg \
+  "https://github-readme-stats.vercel.app/api?username=dan-mba&show_icons=true&title_color=006db0&icon_color=006db0"
 stats=$?
-curl --fail --silent -o langs.svg "https://github-readme-stats.vercel.app/api/top-langs/?username=dan-mba&layout=compact&hide=C%23&title_color=006db0"
+if [$stats -gt 0]
+then
+  echo "Curl for stat failed with rc $stats"
+fi
+curl --fail --silent \
+  -o langs.svg \
+  "https://github-readme-stats.vercel.app/api/top-langs/?username=dan-mba&layout=compact&hide=C%23&title_color=006db0"
 langs=$?
-echo "$stats $langs"
+if [$langs -gt 0]
+then
+  echo "Curl for langs failed with rc $langs"
+fi
